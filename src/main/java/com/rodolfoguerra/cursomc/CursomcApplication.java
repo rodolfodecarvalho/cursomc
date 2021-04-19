72d7d14ba2272e500c8b56c0b0237c747e8a262d
Rodolfo de Carvalho Guerra <rodolfoguerraster@gmail.com>
2021-04-19 00:19:52 -0300

    Operacao de instanciacao

--- src/main/java/com/rodolfoguerra/cursomc/CursomcApplication.java
+++ src/main/java/com/rodolfoguerra/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.rodolfoguerra.cursomc;
 
+import com.rodolfoguerra.cursomc.model.Category;
+import com.rodolfoguerra.cursomc.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args)  {
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
