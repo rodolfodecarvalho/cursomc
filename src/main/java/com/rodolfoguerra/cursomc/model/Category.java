bb45a7621ae26dd6d1c866268be6cb998d5f8eef
Rodolfo de Carvalho Guerra <rodolfoguerraster@gmail.com>
2021-04-18 23:20:41 -0300

    Banco de dados H2 local

--- src/main/java/com/rodolfoguerra/cursomc/model/Category.java
+++ src/main/java/com/rodolfoguerra/cursomc/model/Category.java
@@ -2,9 +2,13 @@ package com.rodolfoguerra.cursomc.model;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
@@ -14,6 +18,7 @@ public class Category implements Serializable {
     private static final long serialVersionUID = 8213987281589035882L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
 
     String name;
